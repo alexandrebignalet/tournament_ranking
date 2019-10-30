3a66020741e98a375e67fb0d499abb224d212955
alexandrebignalet <alexandre.bignalet@gmail.com>
2019-10-30 10:45:28 +0100

    Created a service to fetch leaderboard Expose data in component and display it.

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Competitor, CompetitorsApiClientService} from "./competitors-api-client.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'webapp';
+  title = 'Tournament Ranking';
+  private competitors: Competitor[] = [];
+
+  constructor(private competitorsApiClient: CompetitorsApiClientService) {}
+
+  ngOnInit() {
+    this.getCompetitors()
+  }
+
+  getCompetitors(): void {
+    this.competitorsApiClient.getCompetitors()
+      .subscribe(competitors => this.competitors = competitors);
+  }
 }
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {APIInterceptor} from "../utils/http-interceptor.provider";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: APIInterceptor,
+    multi: true,
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
